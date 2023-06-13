65e2b383f46e824cfe4e3157ddf59bc41277b4a0
Bryan <bryanpablo472@gmail.com>
2023-06-13 12:00:22 -0700

    css update

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,11 +3,10 @@ import React from 'react';
 const Nav = () => {
   return (
     <nav className="navbar">
-      <div className="container-fluid">
+   
         <div className="navbar-left">
           <a className="navbar-name">Bryan Pablo</a>
-        </div>
-        {/* Add additional navbar items here */}
+      
       </div>
     </nav>
   );
