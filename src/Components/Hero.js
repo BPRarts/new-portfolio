1b0af9e1f1a5f078428ac594dd8af5c7a801fcde
Bryan <bryanpablo472@gmail.com>
2023-06-12 18:33:31 -0700

    css update

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import hero from './Img/portfoliobanner.jpg';
+import face from './Img/portfolio face pic complete.png';
 
 
 const Hero = () => {
     return (
-        <div>
-           <img className = "hero" src={hero} alt= ''/>
+        <div className='hero-container'>
+           <div className='face-container'>
+           <img className = "face" src={face} alt= ''/>
+           </div>
+
+           <div className='me-container'>
+           <h1 className='me-title'>Creative</h1>
+           <h2 className='me-title2'>Full Stack Developer</h2>
+           <p className='me-description'>I'm a passionate and dedicated web developer,
+                always seeking new challenges and opportunities to 
+                expand my knowledge and skills in the field. </p>
+            </div>
+
         </div>
     );
 }
